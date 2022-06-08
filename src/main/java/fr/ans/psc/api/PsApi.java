f46e17ecc2cd41816e47b49e9de08144b123c730
bbonche <bbonche@henix.fr>
2022-06-08 17:14:46 +0200

    add queryParams in GET PS method

--- src/main/java/fr/ans/psc/api/PsApi.java
+++ src/main/java/fr/ans/psc/api/PsApi.java
@@ -247,6 +247,7 @@ public class PsApi {
         final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
         final HttpHeaders headerParams = new HttpHeaders();
         final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "include", include));
 
         final String[] accepts = { 
             "application/json"
