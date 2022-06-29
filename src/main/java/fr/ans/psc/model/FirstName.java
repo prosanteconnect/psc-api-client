e0c7976f59d5292c053a2ae6b89774da662c4ac4
ssviridov.henix <ssviridov@henix.fr>
2022-06-29 15:15:38 +0200

    added equals and hashCode overrides to FirstName, replaced firstNames with firstName in Profession

--- src/main/java/fr/ans/psc/model/FirstName.java
+++ src/main/java/fr/ans/psc/model/FirstName.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FirstName {
 
@@ -38,6 +39,12 @@ public class FirstName {
     this.order = order;
   }
 
+  /**
+   * Transforms a string of first names separated by an apostrophe (')
+   * into a list of FirstName objects, in the same order as they appear in the string.
+   * @param string a string of first names separated by an apostrophe (') and containing exactly FIRST_NAME_COUNT-1 apostrophes (')
+   * @return list of FirstName objects
+   */
   public static List<FirstName> stringToList(String string) {
     String[] firstNameStrings = string.split("'");
     List<FirstName> firstNames = new ArrayList<>();
@@ -47,11 +54,17 @@ public class FirstName {
     return firstNames;
   }
 
+  /**
+   * Transforms a list of FirstName objects into a string of first names separated by an apostrophe ('),
+   * and containing exactly FIRST_NAME_COUNT-1 apostrophes (') even if the list contains less than FIRST_NAME_COUNT names.
+   * @param firstNames list of FirstName objects
+   * @return a string of first names separated by an apostrophe (') and containing exactly FIRST_NAME_COUNT-1 apostrophes (')
+   */
   public static String listToString(List<FirstName> firstNames) {
     StringBuilder stringBuilder = new StringBuilder();
 
-    for ( int i = 0; i < FIRST_NAME_COUNT - firstNames.size(); i++ ) {
-      if(firstNames.get(i) != null) stringBuilder.append(firstNames.get(i).getFirstName());
+    for ( int i = 0; i < FIRST_NAME_COUNT; i++ ) {
+      if(i < firstNames.size()) stringBuilder.append(firstNames.get(i).getFirstName());
       stringBuilder.append("'");
     }
     stringBuilder.deleteCharAt(stringBuilder.length() - 1);
@@ -62,4 +75,22 @@ public class FirstName {
   public String toString() {
     return this.firstName;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    FirstName firstName = (FirstName) o;
+    return Objects.equals(this.getFirstName(), firstName.getFirstName()) &&
+            Objects.equals(this.getOrder(), firstName.getOrder());
+  }
+
+  @Override
+    public int hashCode() {
+        return Objects.hash(this.getFirstName(), this.getOrder());
+    }
 }
--- src/main/java/fr/ans/psc/model/Profession.java
+++ src/main/java/fr/ans/psc/model/Profession.java
@@ -39,8 +39,8 @@ public class Profession {
   @JsonProperty("lastName")
   private String lastName = null;
 
-  @JsonProperty("firstNames")
-  private List<FirstName> firstNames = null;
+  @JsonProperty("firstName")
+  private String firstName = null;
 
   @JsonProperty("expertises")
   private List<Expertise> expertises = null;
@@ -138,8 +138,8 @@ public class Profession {
     this.lastName = lastName;
   }
 
-  public Profession firstNames(List<FirstName> firstNames) {
-    this.firstNames = firstNames;
+  public Profession firstNames(FirstName firstNames) {
+    this.firstName = firstName;
     return this;
   }
 
@@ -148,12 +148,12 @@ public class Profession {
     * @return firstName
     **/
   @Schema(description = "")
-  public List<FirstName> getFirstName() {
-    return firstNames;
+  public String getFirstName() {
+    return firstName;
   }
 
-  public void setFirstName(List<FirstName> firstNames) {
-    this.firstNames = firstNames;
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
   }
 
   public Profession expertises(List<Expertise> expertises) {
@@ -223,14 +223,14 @@ public class Profession {
         Objects.equals(this.categoryCode, profession.categoryCode) &&
         Objects.equals(this.salutationCode, profession.salutationCode) &&
         Objects.equals(this.lastName, profession.lastName) &&
-        Objects.equals(this.firstNames, profession.firstNames) &&
+        Objects.equals(this.firstName, profession.firstName) &&
         Objects.equals(this.expertises, profession.expertises) &&
         Objects.equals(this.workSituations, profession.workSituations);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(exProId, code, categoryCode, salutationCode, lastName, firstNames, expertises, workSituations);
+    return Objects.hash(exProId, code, categoryCode, salutationCode, lastName, firstName, expertises, workSituations);
   }
 
 
@@ -244,7 +244,7 @@ public class Profession {
     sb.append("    categoryCode: ").append(toIndentedString(categoryCode)).append("\n");
     sb.append("    salutationCode: ").append(toIndentedString(salutationCode)).append("\n");
     sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
-    sb.append("    firstName: ").append(toIndentedString(firstNames)).append("\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
     sb.append("    expertises: ").append(toIndentedString(expertises)).append("\n");
     sb.append("    workSituations: ").append(toIndentedString(workSituations)).append("\n");
     sb.append("}");
